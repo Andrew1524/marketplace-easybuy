1708bd850448f6c80da9af611d07eec6ed1479d1
Andrew1524 <103788058+Andrew1524@users.noreply.github.com>
2025-01-09 00:51:41 +0200

    Redesigned profile header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import './header.scss';
 import logo from '../../resourses/img/logo/logo.svg'
-import search from '../../resourses/img/icons/search.svg'
-import iconUser from '../../resourses/img/icons/user.svg';
+import fingerPrint from '../../resourses/img/icons/fingerprint.svg';
 import iconLike from '../../resourses/img/icons/favorite.svg';
 import iconCard from '../../resourses/img/icons/shopping.svg';
 
@@ -14,35 +13,30 @@ const Header = () => {
 						<img src={logo} alt="logotypes" />
 					</div>
 					<nav className="header__nav">
+						<ul className='header__info'>
+							<li>
+								<a href="!#">Catalogue</a>
+							</li>
+							<li>
+								<a href="!#">Sale</a>
+							</li>
+						</ul>
 						<div className="header__search">
-							<input className="header__search-input" type="text" placeholder="Search"/>
-						</div>
-						{/* <div className="header__info"> */}
-							<ul className='header__info'>
-								<li>
-									<a href="!#">Catalogue</a>
-								</li>
-								<li>
-									<a href="!#">Sale</a>
-								</li>
-								<li>
-									<a href="!#">Support</a>
-								</li>
-							</ul>
-						{/* </div> */}
-						<div className="header__button">
-							<button className="header__button-btn" > Get Started</button>
+							<label className='header__search-field' htmlFor="">
+								<input className="header__search-input" type="search" placeholder="Search"/>
+								<button className='header__search-button'>Search</button>
+							</label>
 						</div>
 						<div className="header__icons">
 							<ul>
 								<li>
 									<a href="!#">
-										<img src={iconUser} alt="" />
+										<img src={iconLike} alt="" />
 									</a>
 								</li>
 								<li>
 									<a href="!#">
-										<img src={iconLike} alt="" />
+									<img src={fingerPrint} alt="" />
 									</a>
 								</li>
 								<li>
@@ -51,6 +45,9 @@ const Header = () => {
 									</a>
 								</li>
 							</ul>
+							<div className="header__sing">
+								<button className='header__button-sing'>Sing Up</button>
+							</div>
 						</div>
 					</nav>
 				</div>
